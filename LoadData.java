8b349142e352c111f8b447b4773c2b98f7e4139d
zpham1 <79631954+zpham1@users.noreply.github.com>
2021-04-09 01:04:00 -0700

    Tried implementing tab pane

---
+++ LoadData.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class LoadData extends JPanel {
+
+	private JLabel loadLabel;
+	
+	public LoadData() {
+		loadLabel = new JLabel("load data");
+		add(loadLabel);
+	}
+}
